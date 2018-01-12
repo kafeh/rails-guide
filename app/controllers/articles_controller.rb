43ba08d8127e555e89f7cd30d3156e364022025c
kafes <escalanteherrera10@gmail.com>
2018-01-12 01:04:33 -0600

    Ruby guide completed

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@articles = Article.all
+	end
+
+	def show
+	end
+	
+	def new
+		@article = Article.new
+	end
+
+	def edit
+	end
+
+	def create
+		@article = Article.new(article_params)
+		
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'			
+		end		
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'	
+		end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private def find_article
+		@article = Article.find(params[:id])
+	end
+
+	private def article_params
+	params.require(:article).permit(:title, :text)
+end
+end
+
