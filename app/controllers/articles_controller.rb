bce3b231fc98b2df37a0c3d5b6d2ad123011f451
kafes <escalanteherrera10@gmail.com>
2018-01-12 01:20:51 -0600

    C:/Program Files/Git/controllers/articles_controller.rb indentation modified

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,10 +41,10 @@ class ArticlesController < ApplicationController
 
 	private def find_article
 	@article = Article.find(params[:id])
-end
+	end
 
-private def article_params
-params.require(:article).permit(:title, :text)
-end
+	private def article_params
+	params.require(:article).permit(:title, :text)
+	end
 end
 
