4186e41e7fb6087b064283bc7a052b60b9f7373a
kafes <escalanteherrera10@gmail.com>
2018-01-15 00:30:46 -0600

    User_helper added for avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+	def avatar_for(user, options = { size: 80})
+		size = options[:size]
+		if user.avatar?
+			image_tag user.avatar.thumb.url, class: "rounded-circle", width: size
+		else
+			image_tag "default-avatar.jpg", class: "rounded-circle", width: size
+		end
+	end	
 end
